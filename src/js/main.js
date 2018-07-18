e44d6c5fa8112214a4e87e4bd6ecd49ab687ca1c
Shay Novik <shay.novik@gmail.com>
2018-07-18 23:06:35 +0300

    load script with defer instead of async + DOMContentLoaded event

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,50 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  // Hamburger menu
-  let hamburger_icon = document.querySelector("#js-hamburger"),
-    hamburger_menu = document.querySelector("#js-nav-menu"),
-    navigation_links = document.querySelectorAll("#js-nav-menu .link");
+// Hamburger menu
+let hamburger_icon = document.querySelector("#js-hamburger"),
+  hamburger_menu = document.querySelector("#js-nav-menu"),
+  navigation_links = document.querySelectorAll("#js-nav-menu .link");
 
-  // Open/close the menu
-  hamburger_icon.addEventListener("click", () => {
+// Open/close the menu
+hamburger_icon.addEventListener("click", () => {
+  hamburger_menu.classList.toggle("navigation--active");
+  console.log("hamburger clicked")
+});
+
+navigation_links.forEach((link) => {
+  link.addEventListener("click", () => {
     hamburger_menu.classList.toggle("navigation--active");
-    console.log("hamburger clicked")
-  });
+    console.log("link clicked")
+  })
+});
 
-  navigation_links.forEach((link) => {
-    link.addEventListener("click", () => {
-      hamburger_menu.classList.toggle("navigation--active");
-      console.log("link clicked")
-    })
-  });
+class ResponsiveBackgroundImage {
 
-  class ResponsiveBackgroundImage {
+  constructor(element) {
+    try {
+      this.element = element;
+      this.img = element.querySelector('#js-responsive-img');
+      this.src = '';
 
-    constructor(element) {
-      try {
-        this.element = element;
-        this.img = element.querySelector('#js-responsive-img');
-        this.src = '';
+      this.img.addEventListener('load', () => {
+        this.update();
+      });
 
-        this.img.addEventListener('load', () => {
-          this.update();
-        });
-
-        if (this.img.complete) {
-          this.update();
-        }
-      } catch (e) {
-        console.log(e);
+      if (this.img.complete) {
+        this.update();
       }
-    }
-
-    update() {
-      let src = typeof this.img.currentSrc !== 'undefined' ? this.img.currentSrc : this.img.src;
-      if (this.src !== src) {
-        this.src = src;
-        this.element.style.backgroundImage = 'url("' + this.src + '")';
-
-      }
+    } catch (e) {
+      console.log(e);
     }
   }
-  new ResponsiveBackgroundImage(document.querySelector('#js-header'));
-});
+
+  update() {
+    let src = typeof this.img.currentSrc !== 'undefined' ? this.img.currentSrc : this.img.src;
+    if (this.src !== src) {
+      this.src = src;
+      this.element.style.backgroundImage = 'url("' + this.src + '")';
+
+    }
+  }
+}
+
+// Initialize bg image
+new ResponsiveBackgroundImage(document.querySelector('#js-header'));
